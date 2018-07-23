c8cbbedae72efa925beb7cf1075cf877f5720cd2
Tempy <rtemple@ramapo.edu>
2018-07-23 10:45:10 -0400

    Added new MaxValue for Assignment 3

--- TopTen/MaxValue.java
+++ TopTen/MaxValue.java
@@ -13,38 +13,35 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-
 public class MaxValue {
-    // Class to implement the mapper interface
+
     static class MyMapper extends Mapper<LongWritable, Text, IntWritable, Text> {
         // Map interface of the MapReduce job
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            // Get the current line
-            String line = value.toString();
-            String[] linevalues = line.split(",");
-            context.write(new IntWritable(Integer.parseInt(linevalues[0])), new Text(linevalues[1] + "," + linevalues[2]));
+            String[] linevalues = value.toString().split(",");
+
+            IntWritable contextKey = new IntWritable(Integer.parseInt(linevalues[0]));
+            Text contextValue = new Text(linevalues[1] + "," + linevalues[2]); 
+            context.write(contextKey, contextValue);
         }
     }
-    // Class to implement the reducer interface
-    static class MyReducer extends Reducer<IntWritable, Text, IntWritable, IntWritable> {
 
+    static class MyReducer extends Reducer<IntWritable, Text, IntWritable, IntWritable> {
+        //Shared reference to max rating
         float maxrating = Float.MIN_VALUE;
+        //Shared reference to topkey
         IntWritable topKey = new IntWritable(-1);
+        //Reference to movie id
         String movie = " ";
 
-        // Reduce interface of the MapReduce job
         public void reduce(IntWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-            // Vars to hold the highest values
             String[] linevalues;
             float rating;
             
-            // Iterate through each value
             for (Text value: values) {
-                
                 linevalues = value.toString().split(",");
                 rating = Float.parseFloat(linevalues[1]); 
-               
-                // Check if the current rating is more than max_rating
+
                 if (rating > maxrating) {
                     maxrating = rating;
                     topKey = key;
@@ -53,24 +50,24 @@ public class MaxValue {
             }
         }
 
-        @Override
-        public void cleanup(Context context) throws IOException, InterruptedException {    
+        public void cleanup(Context context) throws IOException, InterruptedException {
+            //Write to context after reducer finished    
             context.write(topKey, new IntWritable(Integer.parseInt(movie)));    
         }
     }
-    // Main method
+
     public static void main(String[] args) throws Exception {
-    Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "higest");
-    job.setJarByClass(WorkingTopMovie.class);
-    job.setMapperClass(MyMapper.class);
-    job.setReducerClass(MyReducer.class);
-    job.setMapOutputKeyClass(IntWritable.class);
-    job.setMapOutputValueClass(Text.class);
-    job.setOutputKeyClass(IntWritable.class);
-    job.setOutputValueClass(IntWritable.class);
-    FileInputFormat.addInputPath(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
-  }
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Max Value");
+        job.setJarByClass(MaxValue.class);
+        job.setMapperClass(MyMapper.class);
+        job.setReducerClass(MyReducer.class);
+        job.setMapOutputKeyClass(IntWritable.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setOutputKeyClass(IntWritable.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 } 
